dcdecbe1b437ca48fdaea39d46daeb4f27d29221
raina-lu <raina-lu@ibjapan.jp>
2024-01-09 14:45:19 +0900

    docker設定

---
+++ src/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+    <title>Register</title>
+</head>
+<body>
+<main>
+    <form action="#" method="post">
+        <h1>Sign Up</h1>
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div>
+            <label for="kana">Kana:</label>
+            <input type="text" name="kana" id="kana">
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+        </div>
+        <button type="submit">Register</button>
+        <footer>Already a member? <a href="#">Login here</a></footer>
+    </form>
+</main>
+</body>
+</html>
